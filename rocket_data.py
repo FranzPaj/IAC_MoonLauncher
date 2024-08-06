0c002e1e5927748c46c3adbf746ec13bd4b8b328
javialonso05 <javialonso05@gmail.com>
2024-08-06 11:19:55 +0200

    Added payload mass

--- rocket_data.py
+++ rocket_data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def calculate_fuel_mass(rocket_data: dict, transfer: Transfer, deltaV_override: np.ndarray = None):
+def calculate_fuel_mass(rocket_data: dict, transfer: Transfer, deltaV_override: np.ndarray = None, payload_density=50):
     """
     Calculate the fuel mass of a rocket given the data of the rocket assuming oxidizer refueling is possible.
     :param rocket_data: dict with the following:
@@ -16,7 +16,8 @@ def calculate_fuel_mass(rocket_data: dict, transfer: Transfer, deltaV_override: 
 
     delta_v = transfer.get_transfer_delta_V() if deltaV_override is None else deltaV_override
     mass_ratio = np.exp(delta_v / (rocket_data['Isp'] * 9.81))
-    prop_mass = (rocket_data['propellant_mass'] + rocket_data['empty_mass']) * (1 - 1 / mass_ratio)
+    payload_mass = payload_density * rocket_data['payload_V']
+    prop_mass = (rocket_data['propellant_mass'] + rocket_data['empty_mass'] + payload_mass) * (1 - 1 / mass_ratio)
 
     return rocket_data['propellant_mass'] - prop_mass
 
@@ -29,7 +30,8 @@ Falcon9_v12 = {
     'thrust': 934e3,  # N
     'Isp': 348,  # s
     'burn_time': 397,  # s
-    'OF_ratio': 2.56  # oxidizer to fuel ratio
+    'OF_ratio': 2.56,  # oxidizer to fuel ratio
+    'payload_V': 16 * np.pi * 1.83**2  # m^3
 }
 
 
@@ -39,16 +41,20 @@ if __name__ == '__main__':
     ox_mass = Falcon9_v12['OF_ratio'] * fuel_mass
 
     prop_mass_ratio = (fuel_mass + ox_mass) / Falcon9_v12['propellant_mass']
-    prop_mass_ratio[np.where(prop_mass_ratio > 1)] = 1
+    # prop_mass_ratio[np.where(prop_mass_ratio > 1)] = 1
 
     full_refueling = dV[np.where(prop_mass_ratio < 1)[0][0]]
+
+    no_refueling = np.where(prop_mass_ratio <= 0)[0][0]
+    dV = dV[:no_refueling]
+    prop_mass_ratio = prop_mass_ratio[:no_refueling]
 
     # Plot results
     plt.plot(dV / 1e3, prop_mass_ratio * 100)
     plt.vlines(full_refueling / 1e3, 0, 100, color='black', linestyle='--',
-               label=str(np.round(full_refueling / 1e3, 1)) + ' km/s')
+               label='100% refueling')
 
-    plt.title('Falcon 9 oxidizer refueling capacity')
+    plt.title('Payload density = 50 kg/m3')
     plt.xlabel('Delta-V [km/s]')
     plt.ylabel('Refueling capacity [%]')
     plt.legend()
